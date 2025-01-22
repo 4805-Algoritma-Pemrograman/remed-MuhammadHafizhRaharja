51b86bbe3d0ea10fa70ec1eaacd80f109c8087c9
Hafizh <110800141+MuhammadHafizhRaharja@users.noreply.github.com>
2025-01-22 14:44:07 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,23 @@
-public class Nomor1{
-     public static void main(String[] args) {
+import java.util.Scanner;
+
+public class Nomor1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         
+        System.out.print("Masukkan total kilometer: ");
+        int totalKm = scanner.nextInt();
+        
+        double honorTaksi;
+        
+        if (totalKm < 3) {
+            honorTaksi = 20000;
+        } else {
+            honorTaksi = 20000 + (totalKm - 3) * 10000;
+        }
+        
+        System.out.println("Total km: " + totalKm);
+        System.out.println("Honor taksi:" + honorTaksi);
+        
+        scanner.close();
     }
 }
